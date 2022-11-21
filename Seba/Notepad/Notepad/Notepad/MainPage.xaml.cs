f1d020be483f68737087e7a104f2d5ac75e312a2
SebaProgramista <mrprofessoracc@gmail.com>
2022-11-21 13:13:37 +0100

    Add functionality (not complete)

--- Seba/Notepad/Notepad/Notepad/MainPage.xaml.cs
+++ Seba/Notepad/Notepad/Notepad/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -17,12 +20,24 @@ namespace Notepad
 
         private void btn_save_txt_to_file_Clicked(object sender, EventArgs e)
         {
-
+            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "temp.txt");
+            Trace.WriteLine(filename);
+            if (File.Exists(filename))
+            {
+                File.WriteAllText(filename, editor_notepad.Text);
+                Trace.WriteLine(File.ReadAllText(filename));
+            }
         }
 
         private void btn_read_txt_from_file_Clicked(object sender, EventArgs e)
         {
-
+            string filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "temp.txt");
+            Trace.WriteLine(filename);
+            if (File.Exists(filename))
+            {
+                Trace.WriteLine(File.ReadAllText(filename));
+                editor_notepad.Text = File.ReadAllText(filename);
+            }
         }
     }
 }
