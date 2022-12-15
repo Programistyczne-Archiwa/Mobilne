8ec7b779f1d319c68744a72b9a0b3d4a1a2c9979
sashavodkadrink <hubertbp@op.pl>
2022-12-15 09:03:25 +0100

    Xamarin app

---
+++ Rafal/06_10_mobilne/06_10_mobilne/06_10_mobilne/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _06_10_mobilne
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+        static int i = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            i++;
+            labelus.Text = $"{i} polubień";
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            i--;
+            if(i<0)
+            {
+                i = 0;
+            }
+            else{
+                labelus.Text = $"{i} polubień";
+            }
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
