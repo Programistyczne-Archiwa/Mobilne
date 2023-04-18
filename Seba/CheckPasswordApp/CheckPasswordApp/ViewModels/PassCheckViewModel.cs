b87fa49d7994554023dc2ecc73e263c5cc80f1ee
SebaProgramista <mrprofessoracc@gmail.com>
2023-04-18 14:48:46 +0200

    End Project

--- Seba/CheckPasswordApp/CheckPasswordApp/ViewModels/PassCheckViewModel.cs
+++ Seba/CheckPasswordApp/CheckPasswordApp/ViewModels/PassCheckViewModel.cs
@@ -1,15 +1,17 @@
-﻿using System.Diagnostics;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace CheckPasswordApp.ViewModels
 {
     public class PassCheckViewModel : ViewModelBase
     {
+        private string _complexPassword;
         private string _password;
 
         public PassCheckViewModel()
         {
-            CheckCommand = new CheckPasswordCommand();
+            ResetCommand = new ResetPassword(this);
+            CheckCommand = new CheckPassword(this);
+            Password = "";
         }
 
         public string Password
@@ -22,14 +24,66 @@ namespace CheckPasswordApp.ViewModels
             }
         }
 
+        public string ComplexPassword
+        {
+            get => _complexPassword;
+            set
+            {
+                _complexPassword = value;
+                OnPropertyChanged(nameof(ComplexPassword));
+            }
+        }
+
         public ICommand CheckCommand { get; }
+        public ICommand ResetCommand { get; }
     }
 
-    public class CheckPasswordCommand : CommandBase
+    public class CheckPassword : CommandBase
     {
+        private readonly PassCheckViewModel _passCheckViewModel;
+
+        public CheckPassword(PassCheckViewModel passCheckViewModel)
+        {
+            _passCheckViewModel = passCheckViewModel;
+        }
+
+        private bool CheckNumberContains(string text)
+        {
+            int[] nums = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (var i = 0; i < nums.Length; i++)
+                if (text.Contains(nums[i].ToString()))
+                    return true;
+
+            return false;
+        }
+
         public override void Execute(object parameter)
         {
-            Trace.WriteLine("Clicked");
+            _passCheckViewModel.ComplexPassword = "Hasło jest puste";
+            if (CheckNumberContains(_passCheckViewModel.Password))
+            {
+                if (_passCheckViewModel.Password.Length >= 7)
+                    _passCheckViewModel.ComplexPassword = "Hasło jest dobre";
+                else if (_passCheckViewModel.Password.Length <= 6 && _passCheckViewModel.Password.Length >= 4)
+                    _passCheckViewModel.ComplexPassword = "Hasło jest średnie";
+            }
+
+            if (_passCheckViewModel.Password != "") _passCheckViewModel.ComplexPassword = "Hasło jest słabe";
+        }
+    }
+
+    public class ResetPassword : CommandBase
+    {
+        private readonly PassCheckViewModel _passCheckViewModel;
+
+        public ResetPassword(PassCheckViewModel passCheckViewModel)
+        {
+            _passCheckViewModel = passCheckViewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _passCheckViewModel.Password = "";
         }
     }
 }
--- Seba/CheckPasswordApp/CheckPasswordApp/Views/PassCheckView.xaml.cs
+++ Seba/CheckPasswordApp/CheckPasswordApp/Views/PassCheckView.xaml.cs
@@ -1,28 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CheckPasswordApp.Views
 {
     /// <summary>
-    /// Logika interakcji dla klasy PassCheckView.xaml
+    ///     Logika interakcji dla klasy PassCheckView.xaml
     /// </summary>
     public partial class PassCheckView : UserControl
     {
         public PassCheckView()
         {
             InitializeComponent();
+        }
+
+        private void Password_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext != null) ((dynamic)DataContext).Password = ((PasswordBox)sender).Password;
+        }
+
+        private void ShowPasswordCharsCheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            PasswordHidden.Visibility = Visibility.Collapsed;
+            PasswordVisible.Visibility = Visibility.Visible;
+
+            PasswordVisible.Focus();
+        }
+
+        private void ShowPasswordCharsCheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordHidden.Password = ((dynamic)DataContext).Password;
+            PasswordHidden.Visibility = Visibility.Visible;
+            PasswordVisible.Visibility = Visibility.Collapsed;
+
+            PasswordHidden.Focus();
         }
     }
 }
